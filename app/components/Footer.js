f8cb3c2f8f388f8b6ef5cdf77e32edc1b38b10de
AlfaProgramer <kaddu202325@gmail.com>
2026-01-12 21:32:43 +0300

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer() {
     const year = new Date().getFullYear();
   return (
     <footer className="glass-panel footer">
-      <p>© {year} Final Ödevi Projesi. Tüm hakları saklıdır.</p>
-      <p className="developer-info">Yapan Kişi: Rohat Taş</p>
+      <p>© {year} Web Tabanlı Programlama Final Ödevi Projesi.</p>
+      <p className="developer-info">Rohat Taş</p>
     </footer>
   );
 }
