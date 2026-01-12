62047860344959646af784bf805ebfeebf29f816
AlfaProgramer <kaddu202325@gmail.com>
2026-01-12 21:09:47 +0300

    Create Header.js

---
+++ app/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="glass-panel header">
+      <h1>🌍 Küresel Döviz Takip</h1>
+      <p>Canlı piyasa verileri parmaklarınızın ucunda.</p>
+    </header>
+  );
+}
