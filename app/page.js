1e16e0f88cda11eef6bb9810764737900dfa0190
AlfaProgramer <kaddu202325@gmail.com>
2026-01-12 21:43:20 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,26 +1,7 @@
-
-"use client";
-import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import CurrencyCard from "./components/CurrencyCard";
 
 export default function Home() {
-  const [theme, setTheme] = useState("USD"); 
-
-  const handleCurrencyChange = (currency) => {
-    setTheme(currency);
-  };
-
-  const themeClass = `theme-${theme}`;
-
   return (
-    <div className={`app-wrapper ${themeClass}`}>
-      <Header />
-      <main className="main-content">
-        <CurrencyCard onCurrencyChange={handleCurrencyChange} />
-      </main>
-      <Footer />
-    </div>
+    <CurrencyCard />
   );
 }
