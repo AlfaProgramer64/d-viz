e7f65b264ea22088f65fbcabc1ea8e6b881cd0eb
AlfaProgramer <kaddu202325@gmail.com>
2026-01-12 21:16:00 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
+
+"use client";
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import CurrencyCard from "./components/CurrencyCard";
 
 export default function Home() {
+  const [theme, setTheme] = useState("USD"); 
+
+  const handleCurrencyChange = (currency) => {
+    setTheme(currency);
+  };
+
+  const themeClass = `theme-${theme}`;
+
   return (
-    <CurrencyCard />
+    <div className={`app-wrapper ${themeClass}`}>
+      <Header />
+      <main className="main-content">
+        <CurrencyCard onCurrencyChange={handleCurrencyChange} />
+      </main>
+      <Footer />
+    </div>
   );
 }
